4926a0e7169c2f4798b4582ad61e66cf16c8da0d
akaver <akaver@akaver.com>
2021-09-28 22:47:34 +0300

    data explore
    mean, std and black_pixel calculations

---
+++ LightningFashionMNIST.py
@@ -0,0 +1,121 @@
+import os
+from filelock import FileLock
+
+# __import_lightning_begin__
+import torch
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+from torch.nn import functional as F
+from torchvision.datasets import FashionMNIST
+from torchvision import transforms
+# __import_lightning_end__
+
+import torchvision.transforms.functional as TF
+
+
+# __lightning_begin__
+class LightningFashionMNIST(pl.LightningModule):
+    def __init__(self, config, data_dir=None):
+        super(LightningFashionMNIST, self).__init__()
+
+        self.data_dir = data_dir or os.getcwd()
+
+        self.layer_1_size = config["layer_1_size"]
+        self.layer_2_size = config["layer_2_size"]
+        self.lr = config["lr"]
+        self.batch_size = config["batch_size"]
+
+       
+        self.config = config
+        
+        # mnist images are (1, 28, 28) (channels, width, height)
+        self.layer_1 = torch.nn.Linear(28 * 28, self.layer_1_size)
+        self.layer_2 = torch.nn.Linear(self.layer_1_size, self.layer_2_size)
+        # there are 10
+        self.layer_3 = torch.nn.Linear(self.layer_2_size, 10)
+
+    def forward(self, x):
+        batch_size, channels, width, height = x.size()
+        x = x.view(batch_size, -1)
+
+        x = self.layer_1(x)
+        x = torch.relu(x)
+
+        x = self.layer_2(x)
+        x = torch.relu(x)
+
+        x = self.layer_3(x)
+        x = torch.log_softmax(x, dim=1)
+
+        return x
+
+    @staticmethod
+    def cross_entropy_loss(logits, labels):
+        return F.nll_loss(logits, labels)
+
+    @staticmethod
+    def accuracy(logits, labels):
+        _, predicted = torch.max(logits.data, 1)
+        correct = (predicted == labels).sum().item()
+        accuracy = correct / len(labels)
+        return torch.tensor(accuracy)
+
+    def training_step(self, train_batch, batch_idx):
+        x, y = train_batch
+        
+        black = -0.42421296
+        # apply augmentations tp batch
+        for idx, torch_img in enumerate(x):
+            torch_img = TF.rotate(torch_img, self.config["rotate_image"], fill=black)
+            x[idx] = torch_img
+        
+        logits = self.forward(x)
+        loss = self.cross_entropy_loss(logits, y)
+        accuracy = self.accuracy(logits, y)
+
+        self.log("ptl/train_loss", loss)
+        self.log("ptl/train_accuracy", accuracy)
+        return loss
+
+    def validation_step(self, val_batch, batch_idx):
+        x, y = val_batch
+        logits = self.forward(x)
+        loss = self.cross_entropy_loss(logits, y)
+        accuracy = self.accuracy(logits, y)
+        return {"val_loss": loss, "val_accuracy": accuracy}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        avg_acc = torch.stack([x["val_accuracy"] for x in outputs]).mean()
+        self.log("ptl/val_loss", avg_loss)
+        self.log("ptl/val_accuracy", avg_acc)
+
+    @staticmethod
+    def download_data(data_dir):
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        with FileLock(os.path.expanduser("~/.data.lock")):
+            # https://pytorch.org/vision/stable/datasets.html#fashion-mnist
+            return FashionMNIST(data_dir, train=True, download=True, transform=transform)
+
+    def prepare_data(self):
+        mnist_train = self.download_data(self.data_dir)
+
+        self.mnist_train, self.mnist_val = random_split(
+            mnist_train, [55000, 5000])
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=int(self.batch_size), num_workers=8)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=int(self.batch_size), num_workers=8)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+
+# __lightning_end__
+
